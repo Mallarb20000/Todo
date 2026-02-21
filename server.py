5accb702d0fff8bbaf15b718d6d38c03fd478d79
Rohit Malla <P2779735@my365.dmu.ac.uk>
2026-02-21 04:55:34 +0000

    Add do_POST

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ todo = []
 
 class ToDoHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == "/todo":
+        if self.path == "/todo/GET":
             self.send_response(200)
             self.send_header("Content-Type", "application/json")
             self.end_headers()
@@ -17,6 +17,21 @@ class ToDoHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(json.dumps({"error": "path not found"}).encode())
 
+    def do_POST(self):
+        if self.path == "/todo/POST":
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            todo.append(json.loads((post_data.decode())))
+            self.send_response(201)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps({"message":"Task added sucessfully"}).encode())
+        else:
+            self.send_response(404)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps({"Error":"Path not found"}).encode())
+            
 if __name__ == "__main__":
     server = HTTPServer(("localhost", 8000), ToDoHandler)
     print("Server started at http://localhost:8000")
